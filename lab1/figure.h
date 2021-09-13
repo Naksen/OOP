0bc05b5913a14365497377593cb8ca5cf3f1e1c9
Naksen <90452587+Naksen@users.noreply.github.com>
2021-09-13 13:04:34 +0300

    Add files via upload

---
+++ lab1/figure.h
@@ -0,0 +1,12 @@
+#ifndef OOP_LAB_1_FIGURE_H
+#define OOP_LAB_1_FIGURE_H
+#include "point.h"
+
+class Figure {
+private:
+    virtual void Print(std::ostream& os) = 0;
+    virtual double Area() = 0;
+    virtual size_t VertexesNumber() = 0;
+};
+
+#endif
